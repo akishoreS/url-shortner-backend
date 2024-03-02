12b8a7d87d9036ed008e5ae96d90f324c5465eb7
akishoreS <aryasingh0127@gmail.com>
2024-03-02 15:45:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const  express=require("express");
+// const path = require('path');
+const{connectToMongoDB}=require("./connect");
+const urlRoute = require("./routes/url");
+ 
+const URL = require("./models/url")
+
+const app=express();
+const PORT= 8001;
+
+connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log("Mongodb connected"));
+
+// app.set("view engine","ejs");
+// app.set("views",path.resolve("./views"))
+
+app.use(express.json());
+
+// app.get("/test", async(req,res) => {
+//     const allUrls = await URL.find({});
+//     return res.render("home",{
+//         urls: allUrls,
+//     });
+// })
+app.use("/url",urlRoute);
+
+
+app.get("/:shortId", async(req, res) => {
+    // console.log(req.params);
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId,
+        },
+        {
+            $push:{
+                visitHistory:{
+                    timestamp: Date.now(),
+                },
+            },
+        }
+    );
+    res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT,() => console.log(`Server started at PORT:${PORT}`));
