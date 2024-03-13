2dd0407a3855adb33f96d8e597357db65bcd7b80
akishoreS <aryasingh0127@gmail.com>
2024-03-14 00:36:28 +0530

    minor

--- models/user.js
+++ models/user.js
@@ -3,21 +3,21 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     googleId: {
         type: String,
-        required: false,
+        required: true,
         unique: true, // Acts as a primary key for user identification
     },
     email: {
         type: String,
-        required: false,
+        required: true,
         unique: true,
     },
     name: { // Updated from 'username' to 'name' as per your requirement
         type: String,
-        required: false,
+        required: true,
     },
     imageUrl: { // New field for storing image URL
         type: String,
-        required: false, // Making it optional based on your requirement
+        required: true, // Making it optional based on your requirement
     },
     urls: [{ // Keeping as is, assuming you want to maintain a list of URLs associated with the user
         type: mongoose.Schema.Types.ObjectId,
