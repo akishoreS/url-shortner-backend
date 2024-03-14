0fe7c4c95e495604bae7f84dd4bee95362c6d357
akishoreS <aryasingh0127@gmail.com>
2024-03-14 17:01:09 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+// const express = require('express');
 const cookieParser = require('cookie-parser');
 const express = require("express");
 const { connectToMongoDB } = require("./connect");
